9e7c231a3daf0353eb144b35211ac54209b3d8ac
Anil-Mash <anilchat341.gpt@gmail.com>
2026-04-10 15:15:00 +0530

    Build a Search Filter

--- src/Example1.jsx
+++ src/Example1.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 function Example1() {
   const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -9,10 +10,22 @@ function Example1() {
       .then((data) => setUsers(data));
   }, []);
 
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(search.toLowerCase()),
+  );
+
   return (
     <>
+      <input
+        type="text"
+        placeholder="Search"
+        name="search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
       <ul>
-        {users.map((user) => (
+        {filteredUsers.map((user) => (
           <li key={user.id}> {user.name} </li>
         ))}
       </ul>
