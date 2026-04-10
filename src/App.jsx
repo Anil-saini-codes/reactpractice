1b9d42654108c7f60ad544736d37407a0cf36d26
Anil-Mash <anilchat341.gpt@gmail.com>
2026-04-10 15:34:57 +0530

    Lifting State up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Example1 from "./Example1";
+import LiftingStateUp from "./LiftingStateUp";
 function App() {
   return (
     <>
       <Example1 />
+      <LiftingStateUp />
     </>
   );
 }
